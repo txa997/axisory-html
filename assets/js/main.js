c313b45b631dc3903e1d3cbcc1f8a5f8c09bc885
akmol hosen <akmolhosen997@gmail.com>
2026-02-11 17:55:15 +0600

    h1-40%

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,20 +21,47 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.timeline({
   scrollTrigger: {
-    trigger: ".fold-scene",
+    trigger: ".as-about-1-fold-scene",
     start: "top 75%",
 
     markers: false,
     scrub: 1,
   }
 })
-.to(".left", {
+.to(".as-about-1-fold-scene .left", {
   rotateY: 0,
   duration: 1.4,
   ease: "power3.out"
 })
-.to(".right", {
+.to(".as-about-1-fold-scene .right", {
   rotateY: 0,
   duration: 1.4,
   ease: "power3.out"
+}, "<")
+.from(".as-about-1-fold-scene-line", {
+  scaleX: 0,
+  duration: 1.4,
+  ease: "power3.out"
 }, "<");
+
+
+$(document).on('click', '.wa_toggle_active', function () {
+
+	if ($(this).hasClass('active')) {
+		$(this).removeClass('active');
+	} else {
+		$(this).addClass('active').siblings().removeClass('active');
+	}
+
+});
+
+
+document.querySelectorAll('.as-services-1-tabs-btn .nav-link')
+.forEach(tab => {
+
+    tab.addEventListener('mouseenter', function () {
+        const bsTab = new bootstrap.Tab(this);
+        bsTab.show(); // hover এ tab change
+    });
+
+});
